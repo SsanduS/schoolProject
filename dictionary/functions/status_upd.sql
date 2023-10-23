7ddb8e47861156914e9410c656ced07bddd74eaf
sandu <vahnovansandu@gmail.com>
2023-10-23 17:06:47 +0300

    dictionary function upd

---
+++ dictionary/functions/status_upd.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION status_upd(_src JSONB) RETURNS JSONB
+    SECURITY DEFINER
+    LANGUAGE plpgsql
+as
+$$
+DECLARE
+BEGIN
+    insert into dictionary.status(status_id, statusname)
+    SELECT COALESCE(st.status_id, nextval('dictionary.status_status_id_seq')) AS status_id,
+           s.statusname
+    FROM JSONB_TO_RECORD(_src) AS s (status_id SMALLINT,
+                                     statusname VARCHAR(40))
+             LEFT JOIN dictionary.status st
+                       ON st.status_id = s.status_id
+    ON CONFLICT (status_id) DO UPDATE
+        SET statusname = excluded.statusname;
+
+    RETURN JSONB_BUILD_OBJECT('data', NULL);
+END
+$$;
---
+++ dictionary/functions/subject_upd.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION subject_upd(_src JSONB) RETURNS JSONB
+    SECURITY DEFINER
+    LANGUAGE plpgsql
+as
+$$
+BEGIN
+    INSERT INTO dictionary.subject (subject_id, subjectname)
+    SELECT COALESCE(sub.subject_id, nextval('dictionary.subject_subject_id_seq')) AS subject_id, s.subjectname
+    FROM JSONB_TO_RECORD(_src) AS s (subject_id INT,
+                                     subjectname VARCHAR(70))
+             LEFT JOIN dictionary.subject sub
+                       ON sub.subject_id = s.subject_id
+    ON CONFLICT (subject_id) DO UPDATE
+        SET subjectname = excluded.subjectname;
+
+    RETURN JSONB_BUILD_OBJECT('data', NULL);
+
+END
+$$;
