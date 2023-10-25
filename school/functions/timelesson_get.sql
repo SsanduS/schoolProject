e05b6b63de1ec2db13b9d29db6b3c0d8db150d1d
sandu <vahnovansandu@gmail.com>
2023-10-25 23:05:43 +0300

    кодстайл + varchar для кабинетов

--- school/functions/timelesson_get.sql
+++ school/functions/timelesson_get.sql
@@ -1,4 +1,6 @@
-CREATE OR REPLACE FUNCTION school.timelesson_get(_class_id SMALLINT, _day_id SMALLINT, _lessonnumber SMALLINT, _subject_id SMALLINT, _cabinet_id SMALLINT, _teacher_id SMALLINT, _dt DATE) RETURNS JSONB
+CREATE OR REPLACE FUNCTION school.timelesson_get(_class_id SMALLINT, _day_id SMALLINT, _lessonnumber SMALLINT,
+                                                 _subject_id SMALLINT, _cabinet_id SMALLINT, _teacher_id SMALLINT,
+                                                 _dt DATE) RETURNS JSONB
     SECURITY DEFINER
     LANGUAGE plpgsql
 AS
@@ -18,10 +20,10 @@ BEGIN
               WHERE t.class_id = COALESCE(_class_id, t.class_id)
                 AND t.day_id = COALESCE(_day_id, t.day_id)
                 AND t.lessonnumber = COALESCE(_lessonnumber, t.lessonnumber)
-              AND t.subject_id = COALESCE(_subject_id, t.subject_id)
-              AND t.cabinet_id = COALESCE(_cabinet_id, t.cabinet_id)
-              AND t.teacher_id = COALESCE(_teacher_id, t.teacher_id)
-              AND t.dt = COALESCE(_dt, t.dt)) res;
+                AND t.subject_id = COALESCE(_subject_id, t.subject_id)
+                AND t.cabinet_id = COALESCE(_cabinet_id, t.cabinet_id)
+                AND t.teacher_id = COALESCE(_teacher_id, t.teacher_id)
+                AND t.dt = COALESCE(_dt, t.dt)) res;
 
 END
 $$;
